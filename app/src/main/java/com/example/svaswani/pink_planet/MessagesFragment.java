a15059e5c1245a7e23620b12b659f8f97320941a
Sneha Vaswani <svaswani08@yahoo.com>
2016-02-27 18:50:07 -0500

    Added imageView to messages fragment

--- app/src/main/java/com/example/svaswani/pink_planet/MessagesFragment.java
+++ app/src/main/java/com/example/svaswani/pink_planet/MessagesFragment.java
@@ -1,9 +1,13 @@
 package com.example.svaswani.pink_planet;
 
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -11,15 +15,24 @@ import android.widget.TextView;
  */
 public class MessagesFragment extends Fragment {
 
-    private TextView mExampleView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.content_messagesfragment, container,
+                false);
+
+        return rootView;
+    }
+    
 
     public MessagesFragment() {
 
     }
 
-    @Nullable
-    @Override
-    public View getView() {
-        return mExampleView;
-    }
+//    @Nullable
+//    @Override
+//    public View getView() {
+//
+//        return mExampleView;
+//    }
 }
