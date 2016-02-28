023d7457088dc3ad6886fe9239839e7c55d77a26
Sneha Vaswani <svaswani08@yahoo.com>
2016-02-27 21:49:32 -0500

    Added search image in find friends fragment

--- app/src/main/java/com/example/svaswani/pink_planet/FindFragment.java
+++ app/src/main/java/com/example/svaswani/pink_planet/FindFragment.java
@@ -1,9 +1,12 @@
 package com.example.svaswani.pink_planet;
 
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 /**
@@ -11,15 +14,24 @@ import android.widget.TextView;
  */
 public class FindFragment extends Fragment {
 
-    private TextView mExampleView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.content_findfragment, container,
+                false);
+
+        return rootView;
+    }
+
 
     public FindFragment() {
 
     }
 
-    @Nullable
-    @Override
-    public View getView() {
-        return mExampleView;
-    }
+//    @Nullable
+//    @Override
+//    public View getView() {
+//
+//        return mExampleView;
+//    }
 }
