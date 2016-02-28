89b9eda2db96c97e4067cae1c62cb1e4686e7b64
Sam Heckle <sxh4716@rit.edu>
2016-02-27 20:26:49 -0500

    Linked profile.xml to java

--- app/src/main/java/com/example/svaswani/pink_planet/ProfileFragment.java
+++ app/src/main/java/com/example/svaswani/pink_planet/ProfileFragment.java
@@ -1,9 +1,12 @@
 package com.example.svaswani.pink_planet;
 
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 /**
@@ -12,6 +15,16 @@ import android.widget.TextView;
 public class ProfileFragment extends Fragment {
 
     private TextView mExampleView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.content_messagesfragment, container,
+                false);
+
+        return rootView;
+    }
+
 
     public ProfileFragment() {
 
